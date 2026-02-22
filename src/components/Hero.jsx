5881d9a80bd9f8bc89fcbebc0d2765b08b59ac58
agunawanwibisono <gunawanwbs87@gmail.com>
2026-02-22 16:15:09 +0700

    Pesan baru untuk commit pertama

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">A.gunawan wibisono</span>
+            Hi, I'm <span className="text-[#915EFF]">A.Gunawan Wibisono</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I do
